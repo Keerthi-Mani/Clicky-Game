9b5b8875bda78de95630b2c14448dd0cd81200d1
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-21 12:25:38 -0500

    create-react-app

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => {
+  <footer className="footer">
+    <div className="bottom">Clicky Game! 2019</div>
+  </footer>;
+};
+
+export default Footer;
