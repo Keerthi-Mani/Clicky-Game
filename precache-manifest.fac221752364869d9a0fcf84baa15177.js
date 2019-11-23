198bcd06e082b102f6da020d143d9c551ce40c70
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-22 19:51:27 -0500

    Updates

---
+++ precache-manifest.fac221752364869d9a0fcf84baa15177.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba39e135126c054755c45031330a256e",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a880085a8e29ac42bad8",
+    "url": "/Clicky-Game/static/css/main.5e418a0a.chunk.css"
+  },
+  {
+    "revision": "a2741ff0c0eeefc25243",
+    "url": "/Clicky-Game/static/js/2.09f2ec3a.chunk.js"
+  },
+  {
+    "revision": "a880085a8e29ac42bad8",
+    "url": "/Clicky-Game/static/js/main.129ea57e.chunk.js"
+  },
+  {
+    "revision": "d8e75afaa645032cc4e2",
+    "url": "/Clicky-Game/static/js/runtime-main.7cbe38e7.js"
+  }
+]);
